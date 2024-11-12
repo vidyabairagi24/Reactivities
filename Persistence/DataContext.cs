b279ae9a8623926c1780bfa4db31cc868f784c1e
vidya <vidyabairagi24@gmail.com>
2024-11-12 13:40:05 +0530

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Activity = Domain.Activity;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+    }
+}
