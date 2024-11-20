2251d52cb5eadf148241c1a3ddf3b88f23663b3c
vidya <vidyabairagi24@gmail.com>
2024-11-20 05:51:01 +0530

    End of Section 9

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -6,7 +6,7 @@ const sleep = (delay: number) => {
         setTimeout(resolve, delay)
     })
 }
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:5001/api';
 
 axios.interceptors.response.use(async response => {
     try {
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
+import './app/layout/styles.css'
 import { store, StoreContext } from './app/stores/store'
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
